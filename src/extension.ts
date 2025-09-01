2c8473c0fa837ddeaf8ba2881121060b8040ada4
Shane Hird <shane.hird@csgi.com>
2025-09-01 16:51:43 +1000

    Support ec: prefix

--- src/extension.ts
+++ src/extension.ts
@@ -1,5 +1,5 @@
-import * as vscode from 'vscode';
-import * as vscodelc from 'vscode-languageclient';
+import * as vscode from "vscode";
+import * as vscodelc from "vscode-languageclient";
 
 // Derived from vscode-clangd:
 // https://github.com/llvm-mirror/clang-tools-extra/tree/master/clangd/clients/clangd-vscode
@@ -10,20 +10,25 @@ import * as vscodelc from 'vscode-languageclient';
  * @param defaultValue default value to return if option is not set
  */
 function getConfig<T>(option: string, defaultValue?: any): T {
-  const config = vscode.workspace.getConfiguration('epmlsp');
+  const config = vscode.workspace.getConfiguration("epmlsp");
   return config.get<T>(option, defaultValue);
 }
 
-namespace FetchFunctionRequest {
-export const type =
-    new vscodelc.RequestType<vscodelc.TextDocumentIdentifier, string|undefined,
-                             void, void>('$epmlsp/functionDefn');
-}
+// namespace FetchFunctionRequest {
+//   export const type = new vscodelc.RequestType<
+//     vscodelc.TextDocumentIdentifier,
+//     string | undefined,
+//     void,
+//     void
+//   >("$epmlsp/functionDefn");
+// }
 
 class FileStatus {
   private statuses = new Map<string, any>();
-  private readonly statusBarItem =
-      vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left, 10);
+  private readonly statusBarItem = vscode.window.createStatusBarItem(
+    vscode.StatusBarAlignment.Left,
+    10
+  );
 
   onFileUpdated(fileStatus: any) {
     const filePath = vscode.Uri.parse(fileStatus.uri);
@@ -47,7 +52,9 @@ class FileStatus {
     this.statusBarItem.hide();
   }
 
-  dispose() { this.statusBarItem.dispose(); }
+  dispose() {
+    this.statusBarItem.dispose();
+  }
 }
 
 class EpmlspLanguageClient extends vscodelc.LanguageClient {
@@ -59,8 +66,10 @@ class EpmlspLanguageClient extends vscodelc.LanguageClient {
   // For user-interactive operations (e.g. applyFixIt, applyTweaks), we will
   // prompt up the failure to users.
   logFailedRequest(rpcReply: vscodelc.RPCMessageType, error: any) {
-    if (error instanceof vscodelc.ResponseError &&
-        rpcReply.method === "workspace/executeCommand")
+    if (
+      error instanceof vscodelc.ResponseError &&
+      rpcReply.method === "workspace/executeCommand"
+    )
       vscode.window.showErrorMessage(error.message);
     // Call default implementation.
     super.logFailedRequest(rpcReply, error);
@@ -72,59 +81,75 @@ class EpmlspLanguageClient extends vscodelc.LanguageClient {
  *  your extension is activated the very first time the command is executed
  */
 export function activate(context: vscode.ExtensionContext) {
+  //   const myScheme = "svfunc";
+  //   const myProvider = new (class implements vscode.TextDocumentContentProvider {
+  //     // emitter and its event
+  //     onDidChangeEmitter = new vscode.EventEmitter<vscode.Uri>();
+  //     onDidChange = this.onDidChangeEmitter.event;
 
-  const myScheme = 'svfunc';
-  const myProvider = new class implements vscode.TextDocumentContentProvider {
-    // emitter and its event
-    onDidChangeEmitter = new vscode.EventEmitter<vscode.Uri>();
-    onDidChange = this.onDidChangeEmitter.event;
-
-    async provideTextDocumentContent(uri: vscode.Uri): Promise<string> {
-      const docIdentifier = vscodelc.TextDocumentIdentifier.create(uri.toString());
-      const doc = await epmlspClient.sendRequest(FetchFunctionRequest.type, docIdentifier);
-      return <string>doc;
-    }
-  }
-  context.subscriptions.push(vscode.workspace.registerTextDocumentContentProvider(myScheme, myProvider));
+  //     async provideTextDocumentContent(uri: vscode.Uri): Promise<string> {
+  //       const docIdentifier = vscodelc.TextDocumentIdentifier.create(
+  //         uri.toString()
+  //       );
+  //       const doc = await epmlspClient.sendRequest(
+  //         FetchFunctionRequest.type,
+  //         docIdentifier
+  //       );
+  //       return <string>doc;
+  //     }
+  //   })();
+  //   context.subscriptions.push(
+  //     vscode.workspace.registerTextDocumentContentProvider(myScheme, myProvider)
+  //   );
 
   const epmlsp: vscodelc.Executable = {
-    command : getConfig<string>('path'),
-    args : getConfig<string[]>('arguments')
+    command: getConfig<string>("path"),
+    args: getConfig<string[]>("arguments"),
   };
   const serverOptions: vscodelc.ServerOptions = epmlsp;
 
   const clientOptions: vscodelc.LanguageClientOptions = {
-        // Register the server for epm files
-        documentSelector: [
-            { scheme: 'file', language: 'epm' },
-            { scheme: 'svfunc', language: 'epm' }
-        ],
-        initializationOptions: { epmlspFileStatus: true },
-        // Do not switch to output window when epmlsp returns output
-        revealOutputChannelOn: vscodelc.RevealOutputChannelOn.Never
-    };
+    // Register the server for epm files
+    documentSelector: [
+      { scheme: "file", language: "epm" },
+      { scheme: "svfunc", language: "epm" },
+      { scheme: "ec", language: "epm" },
+    ],
+    initializationOptions: { epmlspFileStatus: true },
+    // Do not switch to output window when epmlsp returns output
+    revealOutputChannelOn: vscodelc.RevealOutputChannelOn.Never,
+  };
 
-  const epmlspClient = new EpmlspLanguageClient('EPM Language Server',
-                                                serverOptions, clientOptions);
-  console.log('EPM Language Server active');
+  const epmlspClient = new EpmlspLanguageClient(
+    "EPM Language Server",
+    serverOptions,
+    clientOptions
+  );
+  console.log("EPM Language Server active");
   context.subscriptions.push(epmlspClient.start());
   const status = new FileStatus();
-  context.subscriptions.push(vscode.window.onDidChangeActiveTextEditor(
-      () => { status.updateStatus(); }));
-  epmlspClient.onDidChangeState(({newState}) => {
+  context.subscriptions.push(
+    vscode.window.onDidChangeActiveTextEditor(() => {
+      status.updateStatus();
+    })
+  );
+  epmlspClient.onDidChangeState(({ newState }) => {
     if (newState == vscodelc.State.Running) {
       // epmlsp starts or restarts after crash.
       epmlspClient.onNotification(
-          'textDocument/epmlsp.fileStatus',
-          (fileStatus) => { status.onFileUpdated(fileStatus); });
+        "textDocument/epmlsp.fileStatus",
+        (fileStatus) => {
+          status.onFileUpdated(fileStatus);
+        }
+      );
     } else if (newState == vscodelc.State.Stopped) {
       // Clear all cached statuses when epmlsp crashes.
       status.clear();
     }
-  })
+  });
   // An empty place holder for the activate command, otherwise we'll get an
   // "command is not registered" error.
-  context.subscriptions.push(vscode.commands.registerCommand(
-      'epmlsp-vscode.activate', async () => {}));
-
+  context.subscriptions.push(
+    vscode.commands.registerCommand("epmlsp-vscode.activate", async () => {})
+  );
 }
